5abbd6cf769460edb8dc8edce634a86b08c35670
Harianantha <hariharan.anantharaman@gmail.com>
2016-05-24 12:14:54 +0530

    first commit

---
+++ HotelValetTelegramBotService/src/main/java/com/cognizant/microservice/demo/HotelValetController.java
@@ -0,0 +1,33 @@
+package com.cognizant.microservice.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/browse")
+public class HotelValetController {
+
+	private final static Logger LOGGER = Logger.getLogger(HotelValetController.class.getName()); 
+    private final AtomicLong counter = new AtomicLong();
+    private final static String CLASSNAME="CategoriesController";
+
+    
+    @RequestMapping(value="/process",method=RequestMethod.POST)
+    public void process(@RequestBody String payload) {
+    	String methodName="process";
+    	LOGGER.entering(CLASSNAME, methodName);
+    	System.out.println("Request body is:"+payload);
+        
+       // return null;
+        
+        
+        
+    }
+   
+    
+}
